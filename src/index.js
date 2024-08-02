db7d5e439e51c8e939dc1e8d411784a6fc7f60d8
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2024-08-02 19:07:54 +0200

    Your commit message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; 
 import "normalize.css";
 import "./index.css";
 import App from "./components/app/app";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
